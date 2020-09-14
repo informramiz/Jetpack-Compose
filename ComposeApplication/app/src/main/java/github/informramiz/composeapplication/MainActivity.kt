bbb76ae2f4e2e4acec634b2fc5213900e6e59a7f
Ramiz Raja <informramiz@gmail.com>
2020-09-14 19:30:55 +0500

    feat: Define a simple view in compose

--- ComposeApplication/app/src/main/java/github/informramiz/composeapplication/MainActivity.kt
+++ ComposeApplication/app/src/main/java/github/informramiz/composeapplication/MainActivity.kt
@@ -2,11 +2,17 @@ package github.informramiz.composeapplication
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.Text
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.unit.dp
 import androidx.ui.tooling.preview.Preview
 import github.informramiz.composeapplication.ui.ComposeApplicationTheme
 
@@ -14,25 +20,31 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeApplicationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            NewsStory()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun NewsStory() {
+    val image = imageResource(R.drawable.header)
+    Column(modifier = Modifier.padding(16.dp)) {
+        Image(
+            image,
+            modifier = Modifier
+                .preferredHeight(180.dp)
+                .fillMaxWidth(),
+            contentScale = ContentScale.Crop
+        )
+        Spacer(modifier = Modifier.preferredHeight(16.dp))
+        Text("A day in rainy water")
+        Text("Paris, France")
+        Text("September 2020")
+    }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun DefaultPreview() {
-    ComposeApplicationTheme {
-        Greeting("Android")
-    }
+fun PreviewGreeting() {
+    NewsStory()
 }
