a0a30f33136937246d43ee630c77682faf4f46d6
Ramiz Raja <informramiz@gmail.com>
2020-09-14 19:39:04 +0500

    feat: Apply material theming

--- ComposeApplication/app/src/main/java/github/informramiz/composeapplication/MainActivity.kt
+++ ComposeApplication/app/src/main/java/github/informramiz/composeapplication/MainActivity.kt
@@ -5,16 +5,16 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.Text
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.setContent
 import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.unit.dp
 import androidx.ui.tooling.preview.Preview
-import github.informramiz.composeapplication.ui.ComposeApplicationTheme
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,18 +28,31 @@ class MainActivity : AppCompatActivity() {
 @Composable
 fun NewsStory() {
     val image = imageResource(R.drawable.header)
-    Column(modifier = Modifier.padding(16.dp)) {
-        Image(
-            image,
-            modifier = Modifier
-                .preferredHeight(180.dp)
-                .fillMaxWidth(),
-            contentScale = ContentScale.Crop
-        )
-        Spacer(modifier = Modifier.preferredHeight(16.dp))
-        Text("A day in rainy water")
-        Text("Paris, France")
-        Text("September 2020")
+    MaterialTheme {
+        val typography = MaterialTheme.typography
+        Column(modifier = Modifier.padding(16.dp)) {
+            Image(
+                image,
+                modifier = Modifier
+                    .preferredHeight(180.dp)
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(16.dp)),
+                contentScale = ContentScale.Crop
+            )
+            Spacer(modifier = Modifier.preferredHeight(16.dp))
+            Text(
+                "A day in rainy water",
+                style = typography.h6
+            )
+            Text(
+                "Paris, France",
+                style = typography.body1
+            )
+            Text(
+                "September 2020",
+                style = typography.body2
+            )
+        }
     }
 }
 
